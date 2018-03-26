678e1d31b11153c56fcd4abf07d0d0a99b5b96df
eluts <eluts15@gmail.com>
2018-03-26 16:14:13 -0700

    fix egress port on private.tf. ICMP rule now functions as intended.w

--- private.tf
+++ private.tf
@@ -19,8 +19,8 @@ resource "aws_security_group" "db" {
       cidr_blocks = ["${var.vpc_cidr}"]
     }
 
-    ingress {
-      from_port = -1
+     ingress {
+      from_port = 8
       to_port = -1
       protocol = "icmp"
       cidr_blocks = ["${var.vpc_cidr}"]
@@ -37,6 +37,13 @@ resource "aws_security_group" "db" {
       from_port = 443
       to_port = 443
       protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 8
+      to_port = 0
+      protocol = "icmp"
       cidr_blocks = ["0.0.0.0/0"]
     }
 
